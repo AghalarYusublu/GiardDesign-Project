0c44ec205c86d005ef8489e5e4328de4c9481549
Aghalar Yusublu <agalaryusublu8330@gmail.com>
2023-08-21 13:46:53 +0200

    update

--- scripts/index.js
+++ scripts/index.js
@@ -30,11 +30,50 @@ function navLinkClick() {
 const intSearch = document.querySelector('#inpt_search');
 const navSearch = document.querySelector('.search');
 
-intSearch.addEventListener('focus', function() {
-    navSearch.classList.add('active');
-});
+// intSearch.addEventListener('focus', function() {
+//     navSearch.classList.add('active');
+// });
 
-intSearch.addEventListener('blur', function() {
-    if (navSearch.val().length == 0)
-        navSearch.classList.remove('active');
-});
+// intSearch.addEventListener('blur', function() {
+//     if (navSearch.val().length == 0)
+//         navSearch.classList.remove('active');
+// });
+
+const loadMoreBtn = document.querySelector("#loadMoreBtn");
+let projectImgs = document.querySelector(".project-imgs");
+let imgs = document.querySelectorAll(".project-imgs figure img");
+
+const projectImgArr = [
+    "images/Photo 2.png",
+    "images/Photo 3.png",
+    "images/Photo 4.png",
+    "images/Photo 5.png",
+    "images/Photo 6.png",
+    "images/Photo 7.png",
+    "images/Photo 8.png",
+    "images/Photo 9.png",
+    "images/Photo 10.png",
+    "images/Photo 11.png",
+]
+
+loadMoreBtn.addEventListener("click", function() {
+
+
+    projectImgArr.forEach(img => {
+        let data = `
+        <figure>
+            <img src="${img}" alt="" data-bs-toggle="modal" data-bs-target="#exampleModal" />
+        </figure>
+        `
+        projectImgs.innerHTML += data;
+    })
+
+})
+
+const modalImg = document.querySelector(".modal-body img");
+
+imgs.forEach(img => {
+    img.addEventListener("click", function() {
+        modalImg.src = this.src;
+    })
+})
